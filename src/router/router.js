71d98370efddfb35653e263afea5f32e4dea5ca8
douglaxetg <douglastovar.g@gmail.com>
2022-05-18 23:16:55 -0400

    adding NoPagesFound route, file and new folders

--- src/router/router.js
+++ src/router/router.js
@@ -4,10 +4,13 @@ import AboutPage from '../modules/pokemon/pages/AboutPage'
 import ListPage from '../modules/pokemon/pages/ListPage'
 import PokemonPage from '../modules/pokemon/pages/PokemonPage'
 
+import NoPagesFound from '../modules/shared/pages/NoPagesFound'
+
 const routes = [
     { path: '/', component: ListPage},
     { path: '/about', component: AboutPage},
     { path: '/id', component: PokemonPage},
+    { path: '/:pathMatch(.*)*', component: NoPagesFound},
 ]
 
 const router = createRouter({
